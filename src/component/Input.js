1bd90a2e775a521eb38d41bbdbb09e3cb38c792a
altontonn <altontonnalumasa@gmail.com>
2024-02-03 17:55:00 +0300

    Create a text area

--- src/component/Input.js
+++ src/component/Input.js
@@ -16,9 +16,9 @@ class InputController extends React.Component {
     return(
       <>
         <h1>Hello</h1>
-        <textarea className="form-control" rows="8" cols="12" value={this.state.input} onChange={this.handleChange}></textarea>
+        <textarea rows="4" cols="50" id="editor" value={this.state.input} onChange={this.handleChange}></textarea>
         <p>Controlled Input: </p>
-        <textarea defaultValue={this.state.input}></textarea>
+        <textarea cols="100" id="preview" defaultValue={this.state.input}></textarea>
       </>
     )
   }
